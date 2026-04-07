f99b5f8045d109f8d5d66c002ba0a08e40f3b7cf
AndresIMD <andres.machuca68@gmail.com>
2026-04-07 18:45:57 -0400

    refactor: reorganize Settings UI with integrated FSI/Ratio calculator - Calculator auto-fills CorrectionFactor and InsulinCarbRatio on input change - User can still manually override calculated values - Merged formula + insulin values into single card with hint labels - Persist slow/rapid insulin doses in AppConfig - Removed manual Apply button (replaced by auto-fill with suppress on load)

--- Hummingbird/ViewModels/SettingsViewModel.cs
+++ Hummingbird/ViewModels/SettingsViewModel.cs
@@ -7,6 +7,7 @@ public class SettingsViewModel : BaseViewModel
 {
     private readonly DataService _dataService;
     private readonly ThemeService _themeService;
+    private bool _suppressAutoFill;
 
     public IReadOnlyList<Models.ThemeDefinition> AvailableThemes => _themeService.AvailableThemes;
 
@@ -35,43 +36,6 @@ public class SettingsViewModel : BaseViewModel
             }
         }
     }
-
-    private string _glucoseTarget = "120";
-    public string GlucoseTarget
-    {
-        get => _glucoseTarget;
-        set
-        {
-            if (SetProperty(ref _glucoseTarget, value))
-                UpdateFormulaPreview();
-        }
-    }
-
-    private string _correctionFactor = "60";
-    public string CorrectionFactor
-    {
-        get => _correctionFactor;
-        set
-        {
-            if (SetProperty(ref _correctionFactor, value))
-                UpdateFormulaPreview();
-        }
-    }
-
-    private string _rangeLow = "70";
-    public string RangeLow { get => _rangeLow; set => SetProperty(ref _rangeLow, value); }
-
-    private string _rangeHigh = "140";
-    public string RangeHigh { get => _rangeHigh; set => SetProperty(ref _rangeHigh, value); }
-
-    private string _rangeVeryHigh = "250";
-    public string RangeVeryHigh { get => _rangeVeryHigh; set => SetProperty(ref _rangeVeryHigh, value); }
-
-    private string _insulinCarbRatio = "10";
-    public string InsulinCarbRatio { get => _insulinCarbRatio; set => SetProperty(ref _insulinCarbRatio, value); }
-
-    private string _nightTarget = "150";
-    public string NightTarget { get => _nightTarget; set => SetProperty(ref _nightTarget, value); }
 
     private string _slowInsulinDose = "";
     public string SlowInsulinDose
@@ -104,14 +68,50 @@ public class SettingsViewModel : BaseViewModel
     private string _calculatedRatio = "--";
     public string CalculatedRatio { get => _calculatedRatio; set => SetProperty(ref _calculatedRatio, value); }
 
-    private bool _canApplyCalculated;
-    public bool CanApplyCalculated { get => _canApplyCalculated; set => SetProperty(ref _canApplyCalculated, value); }
+    private bool _hasCalculatorValues;
+    public bool HasCalculatorValues { get => _hasCalculatorValues; set => SetProperty(ref _hasCalculatorValues, value); }
+
+    private string _glucoseTarget = "120";
+    public string GlucoseTarget
+    {
+        get => _glucoseTarget;
+        set
+        {
+            if (SetProperty(ref _glucoseTarget, value))
+                UpdateFormulaPreview();
+        }
+    }
+
+    private string _correctionFactor = "60";
+    public string CorrectionFactor
+    {
+        get => _correctionFactor;
+        set
+        {
+            if (SetProperty(ref _correctionFactor, value))
+                UpdateFormulaPreview();
+        }
+    }
+
+    private string _insulinCarbRatio = "10";
+    public string InsulinCarbRatio { get => _insulinCarbRatio; set => SetProperty(ref _insulinCarbRatio, value); }
+
+    private string _nightTarget = "150";
+    public string NightTarget { get => _nightTarget; set => SetProperty(ref _nightTarget, value); }
+
+    private string _rangeLow = "70";
+    public string RangeLow { get => _rangeLow; set => SetProperty(ref _rangeLow, value); }
+
+    private string _rangeHigh = "140";
+    public string RangeHigh { get => _rangeHigh; set => SetProperty(ref _rangeHigh, value); }
+
+    private string _rangeVeryHigh = "250";
+    public string RangeVeryHigh { get => _rangeVeryHigh; set => SetProperty(ref _rangeVeryHigh, value); }
 
     private string _formulaPreview = "(Glicemia - 120) / 60 = Dosis";
     public string FormulaPreview { get => _formulaPreview; set => SetProperty(ref _formulaPreview, value); }
 
     public Command SaveCommand { get; }
-    public Command ApplyCalculatedValuesCommand { get; }
 
     public SettingsViewModel(DataService dataService, ThemeService themeService)
     {
@@ -121,19 +121,28 @@ public class SettingsViewModel : BaseViewModel
         _isDarkMode = _themeService.IsDarkMode;
         Title = "Configuración";
         SaveCommand = new Command(async () => await SaveAsync());
-        ApplyCalculatedValuesCommand = new Command(ApplyCalculatedValues);
     }
 
     public async Task LoadAsync()
     {
-        var config = await _dataService.GetConfigAsync();
-        GlucoseTarget = config.GlucoseTarget.ToString();
-        CorrectionFactor = config.CorrectionFactor.ToString();
-        RangeLow = config.RangeLow.ToString();
-        RangeHigh = config.RangeHigh.ToString();
-        RangeVeryHigh = config.RangeVeryHigh.ToString();
-        InsulinCarbRatio = config.InsulinCarbRatio.ToString("F0");
-        NightTarget = config.NightTarget.ToString();
+        _suppressAutoFill = true;
+        try
+        {
+            var config = await _dataService.GetConfigAsync();
+            GlucoseTarget = config.GlucoseTarget.ToString();
+            CorrectionFactor = config.CorrectionFactor.ToString();
+            RangeLow = config.RangeLow.ToString();
+            RangeHigh = config.RangeHigh.ToString();
+            RangeVeryHigh = config.RangeVeryHigh.ToString();
+            InsulinCarbRatio = config.InsulinCarbRatio.ToString("F0");
+            NightTarget = config.NightTarget.ToString();
+            SlowInsulinDose = config.SlowInsulinDose > 0 ? config.SlowInsulinDose.ToString("F0") : "";
+            RapidInsulinDose = config.RapidInsulinDose > 0 ? config.RapidInsulinDose.ToString("F0") : "";
+        }
+        finally
+        {
+            _suppressAutoFill = false;
+        }
     }
 
     private void UpdateFormulaPreview()
@@ -153,23 +162,21 @@ public class SettingsViewModel : BaseViewModel
             CalculatedTDD = $"{tdd:F0} u";
             CalculatedFSI = $"{fsi:F0}";
             CalculatedRatio = $"{ratio:F1}";
-            CanApplyCalculated = true;
+            HasCalculatorValues = true;
+
+            if (!_suppressAutoFill)
+            {
+                CorrectionFactor = CalculatedFSI;
+                InsulinCarbRatio = CalculatedRatio;
+            }
         }
         else
         {
             CalculatedTDD = "--";
             CalculatedFSI = "--";
             CalculatedRatio = "--";
-            CanApplyCalculated = false;
+            HasCalculatorValues = false;
         }
-    }
-
-    private void ApplyCalculatedValues()
-    {
-        if (!CanApplyCalculated) return;
-
-        CorrectionFactor = CalculatedFSI;
-        InsulinCarbRatio = CalculatedRatio;
     }
 
     private async Task SaveAsync()
@@ -182,7 +189,9 @@ public class SettingsViewModel : BaseViewModel
             RangeHigh = int.TryParse(RangeHigh, out var high) ? high : 140,
             RangeVeryHigh = int.TryParse(RangeVeryHigh, out var veryHigh) ? veryHigh : 250,
             InsulinCarbRatio = double.TryParse(InsulinCarbRatio, out var ratio) ? ratio : 10,
-            NightTarget = int.TryParse(NightTarget, out var nightTarget) ? nightTarget : 150
+            NightTarget = int.TryParse(NightTarget, out var nightTarget) ? nightTarget : 150,
+            SlowInsulinDose = double.TryParse(SlowInsulinDose, out var slow) ? slow : 0,
+            RapidInsulinDose = double.TryParse(RapidInsulinDose, out var rapid) ? rapid : 0
         };
 
         await _dataService.SaveConfigAsync(config);
